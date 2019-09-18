18318e942c2b22cd3cf09f0865638ec6af3db78c
wangjie <1341138390@qq.com>
2019-09-18 20:09:35 +0800

    Third commit hello.

--- src/array/Hello.java
+++ src/array/Hello.java
@@ -2,6 +2,6 @@ package array;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello The World!");
+        System.out.println("Hello The Worlds!");
     }
 }
